25723ff6197fbe6a6c64c10b9c5a17749706ab05
katsugen <c.seungwon@gmail.com>
2022-07-27 02:11:08 +0900

    Change media item to movie

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -77,7 +77,7 @@ class PlayerActivity : AppCompatActivity() {
             .build()
             .also { exoPlayer ->
                 viewBinding.videoView.player = exoPlayer
-                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
                 exoPlayer.setMediaItem(mediaItem)
                 exoPlayer.playWhenReady = playWhenReady
                 exoPlayer.seekTo(currentWindow, playbackPosition)
